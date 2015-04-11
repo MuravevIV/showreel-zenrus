5f35ba33324f44b6dfdc36229d7a7e693852d785
Ilya Muravev <ilya.yarn@gmail.com>
2015-04-12 02:02:04 +0400

    Add basic frontend polling for currency rates

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,4 +1,33 @@
 
 $(document).ready(function () {
-    //
+
+    var POLL_PERIOD = 5000;
+
+    var firePeriodically = function () {
+        return Rx.Observable.timer(0, POLL_PERIOD);
+    };
+
+    var pollRates = function () {
+        return $.ajax({
+            url: '/api/rates',
+            dataType: 'text'
+        }).promise();
+    };
+
+    var applyUIChange = function (ratesString) {
+
+        var setUIValue = function (key, value) {
+            $('#' + key).text(value);
+        };
+
+        ratesString.split(';').forEach(function (item) {
+            var pair = item.split(':');
+            setUIValue(pair[0], pair[1]);
+        });
+    };
+
+    // main event chain:
+    firePeriodically()
+        .flatMapLatest(pollRates)
+        .subscribe(applyUIChange);
 });
