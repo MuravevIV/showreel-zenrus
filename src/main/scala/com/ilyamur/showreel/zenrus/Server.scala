3ea4a82473f9748e3602da00f1c9a12b4f63d472
Ilya Muravev <ilya.yarn@gmail.com>
2015-04-12 03:05:35 +0400

    Bugfix: add missing trait

--- src/main/scala/com/ilyamur/showreel/zenrus/Server.scala
+++ src/main/scala/com/ilyamur/showreel/zenrus/Server.scala
@@ -2,11 +2,11 @@ package com.ilyamur.showreel.zenrus
 
 import com.ilyamur.bixbite.finance.yahoo.YahooFinance
 import com.ilyamur.bixbite.http.simple.HttpExecutorSimple
-import com.ilyamur.showreel.zenrus.YahooFinanceApp._
 import com.ilyamur.showreel.zenrus.http.HttpConnectionManagerZenrus
+import com.softwaremill.macwire.Macwire
 import com.twitter.finatra._
 
-object Server extends FinatraServer {
+object Server extends FinatraServer with Macwire {
 
     lazy val httpConnectionManager = wire[HttpConnectionManagerZenrus]
     lazy val httpExecutorSimple = wire[HttpExecutorSimple]
