bae5c7194d3996e8de522726ec426723b56074cf
Ilya Muravev <ilya.yarn@gmail.com>
2015-04-11 23:32:42 +0400

    Add basic (polling) service method for currency rates

--- src/main/scala/com/ilyamur/showreel/zenrus/AppController.scala
+++ src/main/scala/com/ilyamur/showreel/zenrus/AppController.scala
@@ -1,12 +1,27 @@
 package com.ilyamur.showreel.zenrus
 
+import java.util.Currency
+
 import com.ilyamur.bixbite.finance.yahoo.YahooFinance
 import com.twitter.finatra.Controller
 
 class AppController(yahooFinance: YahooFinance) extends Controller {
-    
+
     get("/") { request =>
         render.static("index.html").toFuture
+    }
+
+    get("/api/rates") { request =>
+        val ratesString = yahooFinance
+            .getCurrencyRateMap(Map(
+                "USDRUB" ->(Currency.getInstance("USD"), Currency.getInstance("RUB")),
+                "EURRUB" ->(Currency.getInstance("EUR"), Currency.getInstance("RUB"))
+            ))
+            .map { case (key, value) =>
+                s"${key}:${value}"
+            }
+            .mkString(";")
+        render.plain(ratesString).toFuture
     }
 
     error { request =>
