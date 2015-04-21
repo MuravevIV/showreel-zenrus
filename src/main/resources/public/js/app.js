fe105647705194e70d185423d155e5d955264241
Ilya Muravev <ilya.yarn@gmail.com>
2015-04-21 13:23:11 +0400

    Add basic noise+data d3 chart

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -33,19 +33,195 @@ $(document).ready(function () {
         });
     };
 
-    var buildCharts = function () {
-        _.each($('.rate_chart'), function (rate_chart) {
+    var buildPlots = function () {
+        var plots = _.map($('.rate_chart'), function (rate_chart) {
             var $rate_chart = $(rate_chart);
+            var id = $rate_chart.attr('id');
             var chart = $rate_chart.find('.rickshaw_graph')[0];
             var legend = $rate_chart.find('.rickshaw_legend')[0];
+            return {
+                id: id,
+                graph: new Graph(chart, legend)
+            };
+        });
+        return Rx.Observable.just(plots);
+    };
+
+    var updatePlotRates = function (plots, rates) {
+        _.each(plots, function (plot) {
+            _.each(rates, function (rate) {
+                if (plot.id == rate.key) {
+                    var floatValue = parseFloat(rate.value);
+                    plot.graph.continueData(floatValue);
+                }
+            });
         });
     };
 
-    buildCharts();
-
-    // main event chain:
-    firePeriodically()
+    var obsRates = firePeriodically()
         .flatMapLatest(pollRatesString)
-        .map(decodeRatesString)
+        .map(decodeRatesString);
+
+    obsRates
         .subscribe(applyUIChange);
+
+    /*
+     buildPlots()
+     .subscribe(function (plots) {
+     obsRates.subscribe(function (rates) {
+     updatePlotRates(plots, rates);
+     });
+     });
+     */
+
+    var graphs = [
+        {
+            id: "USDRUB",
+            graph: new D3Graph('#USDRUB')
+        },
+        {
+            id: "EURRUB",
+            graph: new D3Graph('#EURRUB')
+        }
+    ];
+
+    obsRates
+        .subscribe(function (rates) {
+            _.each(graphs, function (graph) {
+                _.each(rates, function (rate) {
+                    if (graph.id == rate.key) {
+                        var floatValue = parseFloat(rate.value);
+                        graph.graph.push(floatValue);
+                    }
+                });
+            });
+        });
 });
+
+var D3Graph = function (selector) {
+
+    var baseWidth = 260;
+    var baseHeight = 140;
+
+    var margin = {top: 20, right: 30, bottom: 20, left: 30};
+
+    var width = baseWidth - margin.left - margin.right;
+    var height = baseHeight - margin.top - margin.bottom;
+
+    var svg = d3.select(selector).select(".rickshaw_graph")
+        .append("svg")
+        .attr("width", baseWidth)
+        .attr("height", baseHeight)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    //
+
+    var now = new Date();
+
+    var dataset = [];
+
+    var m = 720;
+    for (var i = 0; i < m; i++) {
+        dataset.push({
+            t: d3.time.second.offset(now, -5 * (m - i)),
+            v: 45 + (Math.random() * 10)
+        });
+    }
+
+    var xScale = d3.time.scale()
+        .domain([d3.time.hour.offset(now, -1), now])
+        .range([0, width]);
+
+    var goldenRatio = 1.6180;
+    var yMin = _.min(dataset, function (d) { return d.v; }).v;
+    var yMax = _.max(dataset, function (d) { return d.v; }).v;
+    var yDomainHeight = yMax - yMin;
+    var yFullHeight = yDomainHeight * goldenRatio;
+    var yPadding = (yFullHeight - yDomainHeight) / 2;
+    var yScale = d3.scale.linear()
+        .domain([yMin - yPadding, yMax + yPadding])
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient("bottom")
+        .ticks(5);
+
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient("left")
+        .ticks(5);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(0,0)")
+        .call(yAxis);
+
+    //
+
+    var lineFunction = d3.svg.line()
+        .x(function (d, i) {
+            return xScale(d.t);
+        })
+        .y(function (d) {
+            return yScale(d.v);
+        })
+        .interpolate("linear");
+
+    svg.append("path")
+        .attr("class", "line")
+        .attr("stroke", "blue")
+        .attr("stroke-width", 2)
+        .attr("fill", "none")
+        .attr("d", lineFunction(dataset));
+
+    var redraw = function () {
+
+        var now = new Date();
+
+        xScale.domain([d3.time.hour.offset(now, -1), now]);
+
+        var goldenRatio = 1.6180;
+        var yMin = _.min(dataset, function (d) { return d.v; }).v;
+        var yMax = _.max(dataset, function (d) { return d.v; }).v;
+        var yDomainHeight = yMax - yMin;
+        var yFullHeight = yDomainHeight * goldenRatio;
+        var yPadding = (yFullHeight - yDomainHeight) / 2;
+        yScale.domain([yMin - yPadding, yMax + yPadding]);
+
+        var lineFunction = d3.svg.line()
+            .x(function (d, i) {
+                return xScale(d.t);
+            })
+            .y(function (d) {
+                return yScale(d.v);
+            })
+            .interpolate("linear");
+
+        svg.select(".line")
+            .attr("d", lineFunction(dataset));
+
+        svg.select(".x.axis")
+            .transition()
+            .call(xAxis);
+
+        svg.select(".y.axis")
+            .transition()
+            .call(yAxis);
+    };
+
+    this.push = function (newValue) {
+        var now = new Date();
+        dataset.push({
+            t: now,
+            v: newValue
+        });
+        redraw();
+    };
+};
