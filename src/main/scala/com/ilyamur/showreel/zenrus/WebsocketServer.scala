165f84e0a7ab5b8e27fef2a0a1f343825c0430ad
Ilya Muravev <ilya.yarn@gmail.com>
2015-05-01 00:18:59 +0400

    Extract important websocket phases

--- src/main/scala/com/ilyamur/showreel/zenrus/WebsocketServer.scala
+++ src/main/scala/com/ilyamur/showreel/zenrus/WebsocketServer.scala
@@ -8,24 +8,34 @@ import com.twitter.util.Future
 class WebsocketServer {
 
     HttpWebSocket.serve(":8888", new Service[WebSocket, WebSocket] {
+        
         def apply(reqWebSocket: WebSocket): Future[WebSocket] = {
             val broker = new Broker[String]
             val offer = broker.recv
             val respWebSocket = reqWebSocket.copy(messages = offer)
             reqWebSocket.messages.foreach { message =>
-                if (message == "pong") {
-                    println("server: client answered pong")
-                }
+                onReceive(broker, message)
             }
-            //
-            println("server: sending ping to client")
-            broker ! "ping"
-            //
+            onOpen(broker)
+            reqWebSocket.onClose.ensure {
+                onClose(broker)
+            }
             Future.value(respWebSocket)
         }
     })
 
-    def handler(message: String): String = {
-        message.reverse
+    def onOpen(broker: Broker[String]) {
+        println("server: sending ping to client")
+        broker ! "ping"
+    }
+
+    def onReceive(broker: Broker[String], message: String) {
+        if (message == "pong") {
+            println("server: client answered pong")
+        }
+    }
+
+    def onClose(broker: Broker[String])  {
+        //
     }
 }
