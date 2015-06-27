4ed8009bb7ffd5df5e4c4041103afa505b56e598
Ilya Muravev <ilya.yarn@gmail.com>
2015-06-27 14:33:57 +0400

    Fix HealthChecker error handling

--- src/main/scala/com/ilyamur/showreel/zenrus/HealthChecker.scala
+++ src/main/scala/com/ilyamur/showreel/zenrus/HealthChecker.scala
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit
 
 import com.ilyamur.bixbite.http.simple.HttpExecutorSimple
 import org.slf4j.LoggerFactory
+import rx.functions.{Func1, Action1}
 import rx.{Observable, Observer}
 
 class HealthChecker(httpExecutorSimple: HttpExecutorSimple) {
@@ -13,23 +14,27 @@ class HealthChecker(httpExecutorSimple: HttpExecutorSimple) {
     private val PERIOD_SEC = 300
 
     private val _log = LoggerFactory.getLogger(getClass)
+    
+    val errorLogger: Action1[Throwable] = new Action1[Throwable] {
+        override def call(e: Throwable): Unit = {
+            _log.error("", e)
+        }
+    }
 
-    Observable.timer(PERIOD_SEC, PERIOD_SEC, TimeUnit.SECONDS).doOnEach(new Observer[JLong] {
-
-        override def onNext(t: JLong): Unit = {
+    val obsHealthCheck = Observable.timer(PERIOD_SEC, PERIOD_SEC, TimeUnit.SECONDS).map[Unit](new Func1[JLong, Unit]() {
+        override def call(t1: JLong): Unit = {
             val response = httpExecutorSimple.execute(CHECK_URL)
             if (!response.isSuccess) {
                 _log.warn("'{}' request responds code={}", CHECK_URL, response.code)
             }
         }
-
-        override def onCompleted(): Unit = {
-            //
+    }).doOnError(errorLogger)
+            .retryWhen(new RetryWithDelay(5, TimeUnit.SECONDS))
+            .share()
+    
+    obsHealthCheck.subscribe(new Action1[Unit] {
+        override def call(unit: Unit): Unit = {
+            // event sink
         }
-
-        override def onError(e: Throwable): Unit = {
-            _log.error("", e)
-        }
-
-    }).retry().publish().connect()
+    })
 }
